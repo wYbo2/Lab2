fa56191ff890a28419ab032e714d0fddc6ae2b5b
w1llyb <williambubuwybb@gmail.com>
2024-10-21 09:53:09 +0800

    BMI calculator

--- lab2_bmi.py
+++ lab2_bmi.py
@@ -1,10 +1,18 @@
+def main():
+    print("ET0735(DevOps for AIoT)- Lab2 - Introduction to Python")
+    #display_main_menu()
+
 def calc_bmi(height, weight):
     print("Height : " + str(height))
     print("Weight : "+ str(weight))
     bmi = str(weight/(height*height))
     return bmi
 
-result = calc_bmi(weight=57, height=1.5)
+
+if __name__ == "__main__":
+    main()
+    
+result = calc_bmi(weight=57, height=1.73)
 print("Your BMI result is : "+result)
 
 if result < "18.5":
@@ -13,3 +21,4 @@ elif "18.5" <= result <= "25.0":
     print("Normal Weight")
 else:
     print("Over Weight")
+
